4e7e99188b854b4c34b2af757fe16884bbb9abbb
Louys <louysgenius@gmail.com>
2022-08-11 15:59:27 -0400

    Servidor Web Básico con Nodejs y Express

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+
+// Initializations
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+
+// Middlewares
+app.use(express.urlencoded({extended: false}));
+
+// Global Variables
+
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+module.exports = app;
+
