cf173277fbf24032f3d97a65ea1baa998f1e30db
Louys <louysgenius@gmail.com>
2022-08-11 23:16:40 -0400

    Mongoose Modelos de la base de datos Mongodb

---
+++ src/models/Contact.js
@@ -0,0 +1,24 @@
+const {Schema, model} = require('mongoose');
+
+const ContactSchema = new Schema({
+    name: {
+        type: String,
+        requires: true
+    },
+    lastname: {
+        type: String,
+        requires: true
+    },
+    email: {
+        type: String,
+        requires: true
+    },
+    phone: {
+        type: String,
+        requires: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = model('Contact', ContactSchema);
---
+++ src/models/User.js
@@ -0,0 +1,30 @@
+const {Schema, model} = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        requires: true
+    },
+    email: {
+        type: String,
+        requires: true
+    },
+    password: {
+        type: String,
+        requires: true
+    }
+}, {
+    timestamps: true
+});
+
+UserSchema.methods.encrypPassword = async password => {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt); 
+};
+
+UserSchema.methods.matchPassword = function(password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+module.exports = model('User', UserSchema);
