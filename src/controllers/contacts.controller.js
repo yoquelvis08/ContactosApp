af9018c1b037bfd2ee10db48904c3c1b6a9ad615
Louys <louysgenius@gmail.com>
2022-08-12 08:34:09 -0400

    Renderizando Lista Contactos a traves de Handlebars

--- src/controllers/contacts.controller.js
+++ src/controllers/contacts.controller.js
@@ -1,16 +1,28 @@
 const contactsCtrl = {};
+const Contact = require('../models/Contact');
 
 contactsCtrl.renderContactForm = (req, res) => {
     res.render('contacts/new-contact');
 };
 
-contactsCtrl.createNewContact = (req, res) => {
-    console.table(req.body)
-    res.send('new contact')
+contactsCtrl.createNewContact = async (req, res) => {
+    const {name, lastname, email, phone} = req.body;
+
+    const newContact = new Contact({
+        name,
+        lastname,
+        email,
+        phone
+    });
+
+    await newContact.save();
+
+    res.send('new contact');
 }
 
-contactsCtrl.renderContacts = (req, res) => {
-    res.send('render contacts')
+contactsCtrl.renderContacts = async (req, res) => {
+    const contacts = await Contact.find().lean();
+    res.render('contacts/all-notes', { contacts });
 }
 
 contactsCtrl.renderEditForm = (req, res) => {
